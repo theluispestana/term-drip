7d5fad8ba66147861a65c499fd1f65437c903380
theluispestana <theluispestana@gmail.com>
2020-06-29 00:04:05 -0400

    created basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Build from "containers/build";
+// import ReactDom from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Build />
-      </div>
+      <Router>
+        <Route exact path="/build" component={Build} />
+      </Router>
     );
   }
 }
