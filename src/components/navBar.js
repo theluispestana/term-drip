34019de0d321df25f01cd04652c913d530124be8
theluispestana <theluispestana@gmail.com>
2020-07-04 21:50:26 -0400

    created and styled navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "styles/navBar.css";
 
 const NavBar = () => {
   return (
-    <div>
+    <div id="nav-bar">
       <h1>NavBar</h1>
+      <nav>
+        <ul>
+          <li>Build</li>
+          <li>Browse</li>
+          <li>Sign Up</li>
+        </ul>
+      </nav>
     </div>
   );
 };
