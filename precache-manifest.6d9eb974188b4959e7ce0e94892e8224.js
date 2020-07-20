9e737bc6934984ca90e2b7e3a7b5f968db61bb0d
theluispestana <theluispestana@gmail.com>
2020-07-19 23:27:34 -0400

    Updates

--- precache-manifest.6d9eb974188b4959e7ce0e94892e8224.js
+++ precache-manifest.6d9eb974188b4959e7ce0e94892e8224.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "171a6798a9b8495749c3cb984bf4900e",
+    "revision": "0d3cbaae761a566751819666c6bb783d",
     "url": "/term-drip/index.html"
   },
   {
-    "revision": "98ec9198331dc71838db",
+    "revision": "4e27b1e2512ecdea9c3e",
     "url": "/term-drip/static/css/main.c5d71fb3.chunk.css"
   },
   {
-    "revision": "709952bd42805ee227c7",
-    "url": "/term-drip/static/js/2.2f5471b7.chunk.js"
+    "revision": "a7e91babd223780bfdc2",
+    "url": "/term-drip/static/js/2.dbe884f1.chunk.js"
   },
   {
     "revision": "a636ddd461bd633ab63e86040460d655",
-    "url": "/term-drip/static/js/2.2f5471b7.chunk.js.LICENSE.txt"
+    "url": "/term-drip/static/js/2.dbe884f1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "98ec9198331dc71838db",
-    "url": "/term-drip/static/js/main.3459e054.chunk.js"
+    "revision": "4e27b1e2512ecdea9c3e",
+    "url": "/term-drip/static/js/main.591b4fbc.chunk.js"
   },
   {
     "revision": "f377d26b75b4cc19694d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/term-drip/precache-manifest.e33178764d2e77ef652b626b4f1cf10a.js"
+  "/term-drip/precache-manifest.6d9eb974188b4959e7ce0e94892e8224.js"
 );
 
 self.addEventListener('message', (event) => {
